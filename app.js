abd43fff278f901c3fb973d05837f29963d0083d
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-28 21:28:18 +0000

    Update

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const colortwo = document.getElementById('colortwo');
 const gradient = document.getElementById('bodybg');
 const showdata = document.getElementById('showvalue');
 const showCBtn = document.getElementById('copyBtn');
-//showdata.addEventListener('click', copyToSwatch);
 colorone.addEventListener('input', setbgColor);
 colortwo.addEventListener('input', setbgColor);
+
+// Create new elements for swatch
+function createSwatch () {
+    showdata.textContent = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
+    let gradient = showdata.textContent;
+    let range = document.createRange();
+    range.selectNode(document.getElementById("showvalue"));
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    let newDiv = document.createElement('div');
+    newDiv.classList.add("col-md-3", "bg-gradient");
+    let newSwatch = document.querySelector('.row');
+    newSwatch.appendChild(newDiv);
+}
 
 // Pick and Set the BG Gradient
 function setbgColor () {
