686b34f546e971c3683c28ca269fc7e8c84f7eba
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-29 09:12:58 +0100

    Update

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const showCBtn = document.getElementById('copyBtn');
 colorone.addEventListener('input', setbgColor);
 colortwo.addEventListener('input', setbgColor);
 
-// Create new elements for swatch
+// Pick and Set the BG Gradient
+function setbgColor () {
+    bodybg.style.background = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
+    showdata.style.display = "none";
+    showCBtn.style.display = "block";
+}
+
+// Copy gradient, Cceate new elements for swatch and add to swatch
 function createSwatch () {
     showdata.textContent = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
     let gradient = showdata.textContent;
@@ -20,29 +27,14 @@ function createSwatch () {
     window.getSelection().removeAllRanges();// to deselect
     swal(`${gradient}`);
     let newDiv = document.createElement('div');
-    newDiv.classList.add("col-md-3", "bg-gradient");
+    newDiv.classList.add("col-md-3");
+    newDiv.setAttribute("id", "bg-gradient");
     let newSwatch = document.querySelector('.row');
     newSwatch.appendChild(newDiv);
+    let swatch = document.getElementById("bg-gradient");
+    swatch.style.backgroundImage = gradient;
     console.log(gradient);
-    //newDiv.style.backgroundColor = "${gradient}";
 }
 
-// Pick and Set the BG Gradient
-function setbgColor () {
-    bodybg.style.background = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
-    showdata.style.display = "none";
-    showCBtn.style.display = "block";
-}
 
-// Copy the  value to the swatch
-function copyToSwatch () {
-    showdata.textContent = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
-    let gradient = showdata.textContent;
-    let range = document.createRange();
-    range.selectNode(document.getElementById("showvalue"));
-    window.getSelection().removeAllRanges(); // clear current selection
-    window.getSelection().addRange(range); // to select text
-    document.execCommand("copy");
 
-    swal(`${gradient}`);
-    }
