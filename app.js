95a91f91fa475e0cdaec17ea081c4ea954812fa2
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-29 12:22:08 +0100

    Update

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-// Global variables & listeners
+// Global variables, listeners & buttons
+
 const colorone = document.getElementById('colorone');
 const colortwo = document.getElementById('colortwo');
 const gradient = document.getElementById('bodybg');
 const showdata = document.getElementById('showvalue');
-const showCBtn = document.getElementById('copyBtn');
+const showCopyBtn = document.getElementById('copyBtn');
+const editBtn = document.createElement("button");
 const delBtn = document.createElement("button");
+editBtn.classList.add('btn', 'btn-warning');
+editBtn.innerHtml = 'Edit';
+editBtn.addEventListener('click', editSwatch());
+
 delBtn.classList.add('btn', 'btn-danger');
 delBtn.innerHtml = 'Delete';
 delBtn.addEventListener('click', deleteSwatch());
@@ -16,23 +22,26 @@ colortwo.addEventListener('input', setbgColor);
 function setbgColor () {
     bodybg.style.background = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
     showdata.style.display = "none";
-    showCBtn.style.display = "block";
+    showCopyBtn.style.display = "block";
 }
 // Copy gradient, Create new elements for swatch and add to swatch
 function createSwatch () {
     showdata.textContent = `linear-gradient(to right, ${colorone.value}, ${colortwo.value})`;
     let gradient = showdata.textContent;
+    let hexValues = `${colorone.value} - ${colortwo.value}`;
     let range = document.createRange();
     range.selectNode(document.getElementById("showvalue"));
     window.getSelection().removeAllRanges(); // clear current selection
     window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
-    swal(`${gradient}`);
+    swal(`${hexValues}`);
     let newDiv = document.createElement('div');
     newDiv.classList.add("col-md-3");
     newDiv.setAttribute("id", "bg-gradient");
+    newDiv.innerHTML = `<h4><input type='button' onclick='add_text()' value='Set Title' /><br />${hexValues}`;
     let newSwatch = document.querySelector('.row');
+
     newSwatch.appendChild(newDiv);
     let swatch = document.getElementById("bg-gradient");
     swatch.style.backgroundImage = gradient;
@@ -40,6 +49,9 @@ function createSwatch () {
 }
 function deleteSwatch() {
     console.log("Delete Works");
+}
+function editSwatch() {
+    console.log('Edit Works');
 }
 
 
